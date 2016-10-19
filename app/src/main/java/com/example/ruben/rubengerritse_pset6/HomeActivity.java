9c4edec15229b1ad4816969997f2a9c8067fd71f
Ruben <rgerritse95@gmail.com>
2016-10-19 18:39:15 +0200

    error fixed

--- app/src/main/java/com/example/ruben/rubengerritse_pset6/HomeActivity.java
+++ app/src/main/java/com/example/ruben/rubengerritse_pset6/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.ruben.rubengerritse_pset6;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HomeActivity extends MenuActivity {
 
     @Override
